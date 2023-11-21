b4997e9108075beebd52ec62fe6850819a6d5333
rgpv bhopal <139623664+Kush-Development@users.noreply.github.com>
2023-11-21 19:28:10 +0530

    Add files via upload
    updated file

--- script.js
+++ script.js
@@ -14,4 +14,32 @@
 
 
 
- 
+ var mon  = document.querySelector("#container1")
+ var love1  = document.querySelector("j")
+
+
+
+ mon.addEventListener("dblclick", function(){
+    love1.style.transform='translate(-50%, -50% )  scale(1)'
+
+    setTimeout(function(){
+        love1.style.transform='translate(-50%, -50% )  scale(0)'
+
+    },2000)
+ }) 
+
+
+
+ var son  = document.querySelector("#container2")
+ var love2  = document.querySelector("k")
+
+
+
+ son.addEventListener("dblclick", function(){
+    love2.style.transform='translate(-50%, -50% )  scale(1)'
+
+    setTimeout(function(){
+        love2.style.transform='translate(-50%, -50% )  scale(0)'
+
+    },2000)
+ }) 
